746a0f5708cc436c5222d3177c44b4ac0662d29a
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2023-01-25 00:19:28 -0600

    Variables

---
+++ 00_hello.py
@@ -0,0 +1,10 @@
+#Hola Mundo
+print('Hello Python')
+
+#Consultar el tipo de dato
+print(type('Hello Python')) #Type 'Str'
+print(type(5)) #Type 'int'
+print(type(1.7)) #Type 'float'
+print(type(True)) #Type 'bool'
+print(type(3 + 3j)) #Type 'complex'
+
---
+++ 01.Variable.py
@@ -0,0 +1,24 @@
+#Variables
+my_variables = 'My String Variable'
+print(my_variables)
+
+my_int_variables = 5
+print(my_variables)
+
+my_bool_variable = False
+print(my_variables)
+
+print(my_variables, my_int_variables, my_bool_variable)
+
+#FUNCIONES DEL SISTEMA#
+#LEN()
+print(len('Isaac the Master'))
+
+print(f'This is the value: {my_int_variables}')
+
+#Variables en una sola linea 
+name, surname, alias, age = 'Isaac', 'Moure', 'MoureDev', 20
+print(f'My name is: {name} {surname}, I have {age} age. And my As is: {alias}.')
+
+
+
